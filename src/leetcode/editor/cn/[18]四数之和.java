b66a2258e8c5a0799ebce79c1279b078c4ad3e98
syl <Syl.A@bupt.cn>
2026-06-11 22:40:34 +0800

    四数之和

---
+++ src/leetcode/editor/cn/[18]四数之和.java
@@ -0,0 +1,110 @@
+//给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[
+//b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）： 
+//
+// 
+// 0 <= a, b, c, d < n 
+// a、b、c 和 d 互不相同 
+// nums[a] + nums[b] + nums[c] + nums[d] == target 
+// 
+//
+// 你可以按 任意顺序 返回答案 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,0,-1,0,-2,2], target = 0
+//输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,2,2,2,2], target = 8
+//输出：[[2,2,2,2]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 2186 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        /**双指针+排序
+         参考三数之和和最接近的三数之和的解法。
+         [避免枚举重复]
+         每一重循环枚举到的元素不小于其上一重循环枚举到的元素，且在同一重循环中不能多次枚举相同的元素
+         1、每一重循环枚举到的下标必须大于上一重循环枚举到的下标
+         2、同一重循环中国，如果当前元素与上一个元素相同，则跳过该元素
+         前两重循环分别枚举前两个数，在两重循环枚举到的数之后使用双指针枚举剩下的两个数。假设两重循环枚举到的前两个数分别位于下标i和j，其中i<j。初始时，左右指针分别指向下标j+1和n-1.每次计算四个数的和，并进行如下操作：
+         如果和等于target，则将枚举到的四个数加到答案中，然后将左指针右移直到遇到不同的数，将右指针左移直到遇到不同的数；
+         如果和小于target，则将左指针右移一位；
+         如果和大于target，则将右指针左移一位。
+         [剪枝]
+         1、在确定第一个数后，如果nums如果 nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target，说明此时剩下的三个数无论取什么值，四数之和一定大于 target，因此退出第一重循环；
+         2、在确定第一个数之后，如果 nums[i]+nums[n−3]+nums[n−2]+nums[n−1]<target，说明此时剩下的三个数无论取什么值，四数之和一定小于 target，因此第一重循环直接进入下一轮，枚举 nums[i+1]；
+         3、在确定前两个数之后，如果 nums[i]+nums[j]+nums[j+1]+nums[j+2]>target，说明此时剩下的两个数无论取什么值，四数之和一定大于 target，因此退出第二重循环；
+         4、在确定前两个数之后，如果 nums[i]+nums[j]+nums[n−2]+nums[n−1]<target，说明此时剩下的两个数无论取什么值，四数之和一定小于 target，因此第二重循环直接进入下一轮，枚举 nums[j+1]。
+         */
+        List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return quadruplets;
+        }
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return quadruplets;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
