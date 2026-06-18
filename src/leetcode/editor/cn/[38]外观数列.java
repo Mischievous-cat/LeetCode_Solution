27b1d0e514ba8131b89e7b9f88e1cd812231d2c8
syl <Syl.A@bupt.cn>
2026-06-18 14:56:12 +0800

    外观数列

--- src/leetcode/editor/cn/[38]外观数列.java
+++ src/leetcode/editor/cn/[38]外观数列.java
@@ -63,7 +63,30 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String countAndSay(int n) {
-        
+        /*遍历生成
+        * 给定的递归公式定义的数字字符串序列如下：
+        * countAndSay(1)='1';
+        * countAndSay(n)是对countAndSay(n-1)的描述，然后转换成另一个数字字符串
+        * 我们定义字符串Si表示countAndSay(i)，我们如果要求得Sn，则我们需先求出Sn-1，然后按照上述描述的方法生成，
+        * 即从左到右一次扫描字符串Sn-1中连续相同的字符的最大数目，然后将字符的统计数目转化为数字字符串再连接上对应的字符。
+        * 我们已知S1为“1”，我们按照上述方法依次生成S2，S3，...，Sn-1，Sn即可
+        * */
+        String str = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int start = 0;
+            int pos = 0;
+            while(pos < str.length()){// 外层：处理每一组
+                while(pos < str.length() && str.charAt(pos) == str.charAt(start)){// 内层：统计当前组有多少个相同字符
+                    pos++;
+                }
+                // 内层结束后，pos指向下一个不同字符的位置
+                sb.append(Integer.toString(pos-start)).append(str.charAt(start));// 添加“个数+字符”
+                start = pos;// 移动到下一组的开始
+            }
+            str = sb.toString();
+        }
+        return str;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
